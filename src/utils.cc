65e566da7e0421d12a471a7d3db61e2a9680e95c
wettestsock <evilsharkeater@gmail.com>
2024-04-11 14:59:37 -0400

    nothing of note

--- src/utils.cc
+++ src/utils.cc
@@ -19,7 +19,7 @@ void merge_sort(std::vector<std::string>& arr, int left, int middle, int right) 
     int left_range = middle-left+1;
     int right_range = right-middle;
 
-    std::vector<std::string> temp_left(arr.begin() + left, arr.begin() + middle+1);
+    std::vector<std::string> temp_left(arr.begin() + left, arr.begin() + middle);
     std::vector<std::string> temp_right(arr.begin() + 2, arr.begin() + 5);
     std::cout << N<<temp_left;
 };
