cbabbf5b713cec0ab7937d187b319e1726241131
wettestsock <evilsharkeater@gmail.com>
2024-04-11 18:01:10 -0400

    nothing of note

--- src/main.cc
+++ src/main.cc
@@ -124,6 +124,7 @@ int main(int argc, char* argv[]){
         for (size_t i = 0; i < vector_ptr.size(); i++)
         {
             std::cout << *(vector_ptr[i]);
+            delete vector_ptr[i];
         }
         
     
