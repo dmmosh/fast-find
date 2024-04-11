726830a61d3edf1ec73bfcd523402d950ee56397
wettestsock <evilsharkeater@gmail.com>
2024-04-11 14:27:59 -0400

    nothing of note

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,6 @@
 #include "header.h"
 
 int main(){
-    std::cout << "fdkjskl\n";
     std::cout << exec("find . -type f -iname \"*.cc*\" 2>/dev/null");
     return 0;
 }
--- src/utils.cc
+++ src/utils.cc
@@ -4,13 +4,13 @@ std::vector<std::string> exec(const std::string& cmd) {
     std::array<char, 200> buffer;
     std::vector<std::string> result;
     std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"), pclose);
-    if (!pipe) {
+    if (!pipe) { //exception handle
         throw std::runtime_error("popen() failed!");
-    }
+    } 
 
     while (fgets(buffer.data(), static_cast<int>(buffer.size()), pipe.get()) != nullptr) {
-        result.push_back(buffer.data());
-        result.back().resize(result.back().size() - 2);
+        result.push_back(buffer.data()); // pushes the output to the back of the array
+        result.back().resize(result.back().size() - 1); //removes the newline character
     }
     return result;
 }
