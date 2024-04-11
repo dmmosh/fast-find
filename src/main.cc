dfc270ea2f80e5581dae6a1fa1d858ec16a0aa25
wettestsock <evilsharkeater@gmail.com>
2024-04-11 15:19:44 -0400

    MERGE SORT WORKS

--- src/main.cc
+++ src/main.cc
@@ -4,7 +4,7 @@ int main(){
     //std::cout << exec("find . -type f -iname \"*.cc*\" 2>/dev/null");
 
     std::vector <std::string> arr = {"hello", "applfdfdgrve", "tesfvdt", "bancana", "to", "jerrfdsy"};
-    merge_sort(arr); //aa
+    merge_sort(arr); //aad
     std::cout << arr;
     return 0;
 }
