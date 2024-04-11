483239ffd508fe72da9188974c0465e245a24a42
wettestsock <evilsharkeater@gmail.com>
2024-04-11 14:48:38 -0400

    nothing of note

--- src/main.cc
+++ src/main.cc
@@ -3,8 +3,8 @@
 int main(){
     std::cout << exec("find . -type f -iname \"*.cc*\" 2>/dev/null");
 
-    std::vector <std::string> arr = {"hello", "apple", "test", "banana", "tom", "jerry"};
-    merge_sort(arr);
-    std::cout <<arr;
+    const std::vector<int> arr={0,1,2,3,4,5,6,7,8,9,10};
+    std::vector<int> slice(arr.begin()+5, arr.begin()+9);
+    std::cout <<slice;
     return 0;
 }
