c42cbf26fca5bec1314b7b18ca1eca1f8fc09f92
wettestsock <evilsharkeater@gmail.com>
2024-04-11 15:31:32 -0400

    nothing of note

--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,29 @@
 #include "header.h"
 
+/*
+FAST FIND
+cli tool for quickly searching files
+
+manual compile:
+g++ src/main.cc src/utils.cc -o fast-find;
+*/
+
+
 int main(int argc, char* argv[]){
     //std::cout << exec("find . -type f -iname \"*.cc*\" 2>/dev/null");
 
     for (size_t i = 1; i < argc; i++)
     {
+        std::cout << "\nVARIABLE ";
+        int j = 0;
+        while(argv[i][j]){
+            std::cout <<toupper(argv[i][j]);
+            j++;
+        };
+        std::cout << N;
+
         std::vector <std::string> arr = exec("find . -type f -iname \"*"+ std::string(argv[i]) +"*\" 2>/dev/null");
+        std::vector <std::string> arr = exec("find . -type d -iname \"*"+ std::string(argv[i]) +"*\" 2>/dev/null");
         merge_sort(arr); //aad
         std::cout << arr;
     }
