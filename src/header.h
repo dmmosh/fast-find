f6dd878440eb307e80dc7310647ec04c7130c8d0
wettestsock <evilsharkeater@gmail.com>
2024-04-11 14:14:39 -0400

    nothing of note

--- src/header.h
+++ src/header.h
@@ -1,9 +1,19 @@
 #include <cstdio>
 #include <iostream>
+#include <stdlib.h>
 #include <memory>
 #include <stdexcept>
 #include <string>
 #include <array>
+#include <vector>
 
 
-std::string exec(const char* cmd);
+std::vector<std::string> exec(const std::string& cmd);
+
+template<typename T>
+std::ostream& operator<<(std::ostream& out, const std::vector<T>& input){
+        for(const T data: input){
+            out << data;
+        }
+        return out;
+};
--- src/utils.cc
+++ src/utils.cc
@@ -1,14 +1,14 @@
 #include "header.h"
 
-std::string exec(const char* cmd) {
+std::vector<std::string> exec(const std::string& cmd) {
     std::array<char, 128> buffer;
-    std::string result;
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    std::vector<std::string> result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd.c_str(), "r"), pclose);
     if (!pipe) {
         throw std::runtime_error("popen() failed!");
     }
     while (fgets(buffer.data(), static_cast<int>(buffer.size()), pipe.get()) != nullptr) {
-        result += buffer.data();
+        result.push_back(buffer.data());
     }
     return result;
 }
