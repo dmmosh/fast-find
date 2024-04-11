eaf36055c514390939acfc40ec4db08f1bf91b41
wettestsock <evilsharkeater@gmail.com>
2024-04-11 14:14:56 -0400

    nothing of note

--- src/header.h
+++ src/header.h
@@ -13,7 +13,7 @@ std::vector<std::string> exec(const std::string& cmd);
 template<typename T>
 std::ostream& operator<<(std::ostream& out, const std::vector<T>& input){
         for(const T data: input){
-            out << data;
+            out << data << "fdjkl";
         }
         return out;
 };
