d68a6d0c488c912fc3e86cbd386a4f388957bb4c
wettestsock <evilsharkeater@gmail.com>
2024-04-11 15:15:08 -0400

    nothing of note

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,7 @@
 #include "header.h"
 
 int main(){
-    std::cout << exec("find . -type f -iname \"*.cc*\" 2>/dev/null");
+    //std::cout << exec("find . -type f -iname \"*.cc*\" 2>/dev/null");
 
     std::vector <std::string> arr = {"hello", "apple", "test", "banana", "tom", "jerry"};
     merge_sort(arr);
