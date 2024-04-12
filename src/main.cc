98c1c81c0896f6bc819d99bd4fcba918f22cab75
wettestsock <evilsharkeater@gmail.com>
2024-04-11 21:41:27 -0400

    nothing of note

--- src/main.cc
+++ src/main.cc
@@ -142,8 +142,7 @@ int main(int argc, char* argv[]){
             //system( ("cd " + (*(vector_ptr[data_i]))[cd_loc-end_i[data_i]]).c_str() );
             std::string location = (*(vector_ptr[data_i]))[cd_loc-end_i[data_i]]; //the location to cd to
             
-            exec("if [[ -f \"" + location + "\" ]]; then cd $(dirname \"" + location + "\"); else cd \"" + location + "\"; fi;");
-            system("cd .src");
+            std::cout << exec("if [[ -f \"" + location + "\" ]]; then cd $(dirname \"" + location + "\"); else cd \"" + location + "\"; fi;");
 
             break; //exits the loop either way 
         }
