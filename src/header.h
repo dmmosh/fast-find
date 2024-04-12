b29ec2c2c36bd62aba08be3088b7c537f9519cce
wettestsock <evilsharkeater@gmail.com>
2024-04-11 22:12:12 -0400

    nothing of note

--- src/header.h
+++ src/header.h
@@ -3,15 +3,14 @@
 #include <stdlib.h>
 #include <memory>
 #include <stdexcept>
+#include <unistd.h>
 #include <string>
 #include <array>
 #include <string.h>
 #include <filesystem>
 #include <vector>
-#include<bits/stdc++.h> 
 
 #define N '\n'
-#define IS_NUM (isdigit(atoi(input.c_str()))) ? true : false;
 
 // EXECUTES A COMMAND AND RETURNS ITS VECTOR
 std::vector<std::string> exec(const std::string& cmd);
