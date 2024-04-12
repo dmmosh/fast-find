aaac16ea0c52a57ec20ca9d30a230b9494bd04bc
wettestsock <evilsharkeater@gmail.com>
2024-04-11 22:30:22 -0400

    nothing of note

--- src/main.cc
+++ src/main.cc
@@ -142,7 +142,7 @@ int main(int argc, char* argv[]){
             //system( ("cd " + (*(vector_ptr[data_i]))[cd_loc-end_i[data_i]]).c_str() );
             std::string location = (*(vector_ptr[data_i]))[cd_loc-end_i[data_i]]; //the location to cd to
 
-            write_tmp(location, "~./fftmp"); //writes to temp file
+            write_tmp(location, "~/.fftmp"); //writes to temp file
             //std::filesystem::current_path("./src");    // change directory
 
             //std::cout << exec("if [[ -f \"" + location + "\" ]]; then cd $(dirname \"" + location + "\"); else cd \"" + location + "\"; fi;");
