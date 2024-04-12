613a5deb5e3eb33f86fdfaca3d311095802903b2
wettestsock <evilsharkeater@gmail.com>
2024-04-11 21:56:05 -0400

    nothing of note

--- src/main.cc
+++ src/main.cc
@@ -141,9 +141,8 @@ int main(int argc, char* argv[]){
             std::cout << end_i << N;
             //system( ("cd " + (*(vector_ptr[data_i]))[cd_loc-end_i[data_i]]).c_str() );
             std::string location = (*(vector_ptr[data_i]))[cd_loc-end_i[data_i]]; //the location to cd to
-            system("pwd");
-            system("cd $(realpath ./src) && dir");
-            system("pwd");
+
+            std::filesystem::current_path("./src");    // change directory
             //std::cout << exec("if [[ -f \"" + location + "\" ]]; then cd $(dirname \"" + location + "\"); else cd \"" + location + "\"; fi;");
 
             break; //exits the loop either way 
